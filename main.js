3bf3a037081b10ccc8b513c47174b2c0eb3eba7f
Bohooslav <bohooslav@icloud.com>
2020-09-30 16:12:19 +0300

    save window size state and restore it on startapp

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let { app, BrowserWindow } = require("electron")
+const windowStateKeeper = require('electron-window-state')
 
 function createWindow () {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800,
+    fullScreen: true
+  });
+
   // Create the browser window.
   win = new BrowserWindow({
     show: false,
@@ -11,11 +18,21 @@ function createWindow () {
       defaultFontFamily: 'sansSerif',
     },
     darkTheme: true,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
   })
+
+  // Let us register listeners on the window, so we can update the state
+  // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  mainWindowState.manage(win)
+
   // win.webContents.openDevTools()
-  // win.loadURL('http://0.0.0.0:8000')
+  // win.loadURL('http://0.0.0.0:8000', {userAgent: 'Chrome'})
   win.loadURL('https://bolls.life', {userAgent: 'Chrome'})
-  win.maximize()
+  // win.maximize()
   win.setMenuBarVisibility(false)
   win.once('ready-to-show', () => {
     win.show()

77ad71b6051c742cc44cd8e8ef4d83fb9f132c4c
Bohuslav <bpavlisinec@gmail.com>
2023-09-26 21:25:42 +0300

    Safety one time Service Worker clean up

--- main.js
+++ main.js
@@ -1,169 +1,188 @@
-const { app, BrowserWindow, shell, Menu, session, Tray } = require("electron")
-const windowStateKeeper = require('electron-window-state')
+const { app, BrowserWindow, shell, Menu, session, Tray } = require("electron");
+const windowStateKeeper = require("electron-window-state");
+const Store = require("electron-store");
 
-let win, tray, mainWindowState
+let win, tray, mainWindowState;
+
+const store = new Store();
 
 function createTray() {
-  tray = new Tray('build/tray.png')
-  tray.setToolTip('Bolls')
+  tray = new Tray("build/tray.png");
+  tray.setToolTip("Bolls");
 
   let trayMenu = Menu.buildFromTemplate([
-    { label: 'Bolls', click: () => { win.show() } },
-    { type: 'separator' },
     {
-      label: 'New Window',
-      accelerator: 'CmdOrCtrl+shift+N',
-      click: createWindow
+      label: "Bolls",
+      click: () => {
+        win.show();
+      },
+    },
+    { type: "separator" },
+    {
+      label: "New Window",
+      accelerator: "CmdOrCtrl+shift+N",
+      click: createWindow,
     },
     {
-      label: 'New Private Window',
-      accelerator: 'CmdOrCtrl+shift+P',
-      click: createPrivateWindow
+      label: "New Private Window",
+      accelerator: "CmdOrCtrl+shift+P",
+      click: createPrivateWindow,
     },
-    { type: 'separator' },
-    { role:'quit' }
-  ])
-  tray.setContextMenu(trayMenu)
+    { type: "separator" },
+    { role: "quit" },
+  ]);
+  tray.setContextMenu(trayMenu);
 }
 
-const menu = Menu.buildFromTemplate([{
-  label: 'Bolls',
-  submenu: [
-    {
-      label: 'New Window',
-      accelerator: 'CmdOrCtrl+shift+N',
-      click: createWindow
-    },
-    {
-      label: 'New Private Window',
-      accelerator: 'CmdOrCtrl+shift+P',
-      click: createPrivateWindow
-    },
-    { type: 'separator' },
-    { role: 'quit' }
-  ]
-},
-{ role: 'editMenu' },
-{ role: 'viewMenu' },
-{ role: 'windowMenu' }
-])
-Menu.setApplicationMenu(menu)
+const menu = Menu.buildFromTemplate([
+  {
+    label: "Bolls",
+    submenu: [
+      {
+        label: "New Window",
+        accelerator: "CmdOrCtrl+shift+N",
+        click: createWindow,
+      },
+      {
+        label: "New Private Window",
+        accelerator: "CmdOrCtrl+shift+P",
+        click: createPrivateWindow,
+      },
+      { type: "separator" },
+      { role: "quit" },
+    ],
+  },
+  { role: "editMenu" },
+  { role: "viewMenu" },
+  { role: "windowMenu" },
+]);
+Menu.setApplicationMenu(menu);
 
 function windowsCount() {
-  return BrowserWindow.getAllWindows()
-    .filter(b => {
-      return b.isVisible()
-    })
-    .length
-  }
-  
-
-function createWindow() {
-  // Used to offset every new window to avoid overlaping with existing windows
-  let windows_count = windowsCount()
-
-  // Create the browser window.
-  win = new BrowserWindow({
-    show: false,
-    backgroundColor: '#04060c',
-    icon: 'build/icon.png',
-    webPreferences: {
-      sandbox: true,
-      defaultFontFamily: 'sansSerif',
-    },
-    darkTheme: true,
-    x: mainWindowState.x + 64 * (windows_count - 1),
-    y: mainWindowState.y + 64 * (windows_count - 1),
-    width: mainWindowState.width,
-    height: mainWindowState.height,
-  })
-
-  // Let us register listeners on the window, so we can update the state
-  // automatically (the listeners will be removed when the window is closed)
-  // and restore the maximized or full screen state
-  mainWindowState.manage(win)
-
-  // win.webContents.openDevTools()
-  // win.loadURL('http://0.0.0.0:8000', {userAgent: 'Chrome'})
-  win.loadURL('https://bolls.life', { userAgent: "'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'" })
-  // win.setMenuBarVisibility(false)
-  win.once('ready-to-show', () => {
-    win.show()
-  })
-  win.webContents.on('new-window', function (e, url) {
-    e.preventDefault()
-    shell.openExternal(url)
-  })
-
-  win.on('enter-full-screen', () => {
-    win.setAutoHideMenuBar(true)
-  })
-  win.on('leave-full-screen', () => {
-    win.setAutoHideMenuBar(false)
-  })
+  return BrowserWindow.getAllWindows().filter((b) => {
+    return b.isVisible();
+  }).length;
 }
 
-
 function createPrivateWindow() {
-  let private_session = session.fromPartition('private')
+  let private_session = session.fromPartition("private");
 
-  let windows_count = windowsCount()
+  let windows_count = windowsCount();
 
   // Create the browser window.
   let private_win = new BrowserWindow({
     show: false,
-    backgroundColor: '#04060c',
-    icon: 'build/icon.png',
+    backgroundColor: "#04060c",
+    icon: "build/icon.png",
     webPreferences: {
       sandbox: true,
-      defaultFontFamily: 'sansSerif',
-      session: private_session
+      defaultFontFamily: "sansSerif",
+      session: private_session,
     },
     darkTheme: true,
     x: mainWindowState.x + 64 * (windows_count - 1),
     y: mainWindowState.y + 64 * (windows_count - 1),
     width: mainWindowState.width,
     height: mainWindowState.height,
-  })
+  });
 
-  private_win.loadURL('https://bolls.life', { userAgent: "'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'" })
+  private_win.loadURL("https://bolls.life", {
+    userAgent:
+      "'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'",
+  });
 
-  private_win.once('ready-to-show', () => {
-    private_win.show()
-  })
-  private_win.webContents.on('new-window', function (e, url) {
-    e.preventDefault()
-    shell.openExternal(url)
-  })
-  private_win.on('enter-full-screen', () => {
-    private_win.setAutoHideMenuBar(true)
-  })
-  private_win.on('leave-full-screen', () => {
-    private_win.setAutoHideMenuBar(false)
-  })
+  private_win.once("ready-to-show", () => {
+    private_win.show();
+  });
+  private_win.webContents.on("new-window", function (e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+  private_win.on("enter-full-screen", () => {
+    private_win.setAutoHideMenuBar(true);
+  });
+  private_win.on("leave-full-screen", () => {
+    private_win.setAutoHideMenuBar(false);
+  });
+}
+
+function createWindow() {
+  // Used to offset every new window to avoid overlaping with existing windows
+  let windows_count = windowsCount();
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    show: false,
+    backgroundColor: "#04060c",
+    icon: "build/icon.png",
+    webPreferences: {
+      sandbox: true,
+      defaultFontFamily: "sansSerif",
+    },
+    darkTheme: true,
+    x: mainWindowState.x + 64 * (windows_count - 1),
+    y: mainWindowState.y + 64 * (windows_count - 1),
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+  });
+
+  // Let us register listeners on the window, so we can update the state
+  // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  mainWindowState.manage(win);
+
+  // win.webContents.openDevTools()
+  // win.loadURL('http://0.0.0.0:8000', {userAgent: 'Chrome'})
+  win.loadURL("https://bolls.life", {
+    userAgent:
+      "'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'",
+  });
+  // win.setMenuBarVisibility(false)
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+  win.webContents.on("new-window", function (e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+
+  win.on("enter-full-screen", () => {
+    win.setAutoHideMenuBar(true);
+  });
+  win.on("leave-full-screen", () => {
+    win.setAutoHideMenuBar(false);
+  });
 }
 
 app.on("ready", () => {
   mainWindowState = windowStateKeeper({
     defaultWidth: 1000,
     defaultHeight: 800,
-    fullScreen: true
+    fullScreen: true,
   });
-  createWindow()
-  createTray()
-})
+  createWindow();
+  createTray();
+  // I need to clean up all service workers and cache only once
+  const didClear = store.get("didClear");
+  if (!didClear) {
+    session.defaultSession.clearStorageData({ storages: ["serviceworkers"] });
+    session.defaultSession.clearStorageData({ storages: ["caches"] });
+    // now memorize you did this, but localStorage is not available in main process
+    store.set("didClear", "true");
+  }
+});
 
 // Make OSX work same as all other systems
 app.on("window-all-closed", () => {
-  app.quit()
-})
+  app.quit();
+});
 
 app.on("activate", () => {
   // On macOS it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-global.sharedObject = { argv: process.argv }
+global.sharedObject = { argv: process.argv };
